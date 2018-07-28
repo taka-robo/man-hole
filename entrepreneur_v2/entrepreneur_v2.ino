4f9cd505d2a7cc4bb86d6040b3039deca1051bf6
taka-robo <seeker9895@gmail.com>
2018-07-28 20:36:39 +0900

    できた

--- entrepreneur_v2/entrepreneur_v2.ino
+++ entrepreneur_v2/entrepreneur_v2.ino
@@ -1,90 +1,95 @@
 /*************************************
 * My Lidar Lite を扱うためのプログラム
 *************************************/
-
-//#include <SoftwareServo.h>
-//#include <TimerOne.h>
+#include <stdio.h>
+#include <TimerOne.h>
 #include <MsTimer2.h>
 #include <Wire.h>
 #include <LIDARLite.h>
-#include <Servo.h>
+#include "Adafruit_VL53L0X.h"
 #define pi 3.14159265
 #define DIR_PIN 2
 #define STEP_PIN 3
 #define deg2rad pi/180.0
 
 LIDARLite myLidarLite;
-Servo servo;
+Adafruit_VL53L0X ToF = Adafruit_VL53L0X();
 
 volatile int stepCount = 0;
 volatile int roundCount = 0;
 
-int height = 0;
-
 void setup() {
   Serial.begin(115200);
   Serial.println("setup");
+  /*ToF set up*/
+  if (!ToF.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }else{
+      Serial.println("ToF ready");
+  }
 
+  /*Lidar set up*/
   myLidarLite.begin(0, true);
   myLidarLite.configure(0);
-
-  servo.attach(11);
-  servo.write(height);
-
+  Serial.println("lidar ready");
+  //step motor set up
   pinMode(DIR_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
-
   digitalWrite(DIR_PIN, HIGH);
-
-  //Timer1.initialize(2000);//2000
-  //Timer1.attachInterrupt(tick);//ステッピングはタイマーで割り込み事によってスムーズに動かす
-  MsTimer2::set(2, tick);
+  /*timer attch set up*/
+  MsTimer2::set(20, tick);/*[ms]*/
   MsTimer2::start();
 }
-
 void loop() {
-  receive(false);
 }
-
-void receive(bool bias)
+int receive()
 {
-
-  // float azimuth = (((float)stepCount / 200.0f) * 360.0f);
-  float azimuth = 1.8 * stepCount * deg2rad;
-  float elevation = (90 - height) * deg2rad;
-  float radius = myLidarLite.distance(1);//極座標系(距離)
-  //極座標を直行座標に変換
-  float y = radius * sin(elevation ) * cos(azimuth );
-  float x = radius * sin(elevation ) * sin(azimuth );
-  float z = radius * cos(elevation );
-  Serial.println(String(-x, 5) + " " + String(-y, 5) + " " + String(z, 5));
-  servo.write(height);
+  VL53L0X_RangingMeasurementData_t measure;
+  ToF.rangingTest(&measure, false); // pass in 'true' to get debug data printout
+  int rad_from_lidar = myLidarLite.distance(1);/*[mm]*/
+  int rad_from_ToF   = measure.RangeMilliMeter;/*[mm]*/
+  /*暫定的にlidarが1000mm以下の場合ToFの値をリターン*/
+  if(rad_from_lidar>1000){
+    return rad_from_lidar;
+  }else{
+    return rad_from_ToF;
+  }
 }
-
-
-void tick()//タイマで割り込む
-{
+int stepUp(){
   digitalWrite(STEP_PIN, HIGH);
   delay(1);
   digitalWrite(STEP_PIN, LOW);
-
   stepCount++;
   if (stepCount >= 200)
   {
     stepCount = 0;
     roundCount++;
   }
-
-  if (roundCount >= 2) //二週したら0に戻す
-  {
-    height = height + 1;//サーボの角度をプラス
-    if (height >= 91)  { //サーボが90度までいったら0に戻す
-      height = 0;
-    }
-    roundCount = 0;
-  }
+  return stepCount;
 }
 
+void tick()//タイマで割り込む
+{
+  interrupts();
+  int radius = receive();
+  noInterrupts();
+  digitalWrite(STEP_PIN, HIGH);
+  delay(1);
+  digitalWrite(STEP_PIN, LOW);
+  stepCount++;
+  if (stepCount >= 200)
+  {
+    stepCount = 0;
+    roundCount++;
+  }
+  //printf("step=%d,rad=%d\n",stepCount,radius);
+  Serial.print(stepCount);
+  Serial.print(",");
+  Serial.println(radius);
+}
 void noneAction() {
  //何の働きもない関数です。 
 }
+
+
